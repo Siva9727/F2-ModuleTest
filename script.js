840cc08a709ea5e956c45abc93a92573a573950b
Siva9727 <sivasakthi203023@gmail.com>
2023-02-01 19:23:04 +0530

    second img updated

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
+const form = document.getElementById("form");
 const formContainer = document.getElementById("formContainer");
-const img1 = document.getElementById("img1");
+const display = document.getElementById("display");
+const users = [];
 
-img1.addEventListener("click", function () {
-    formContainer.style.display = "block";
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const username = document.getElementById("username").value;
+
+  users.push({ name, username });
 });
 
-// once second image is clicked. it should provide the name and username provided on the form. write code for that ?
-//define a variable ?
+document.getElementById("img1").addEventListener("click", function () {
+  formContainer.style.display = "block";
+});
 
 document.getElementById("img2").addEventListener("click", function () {
-    let name = document.getElementById('name').value;
-    let username = document.getElementById("username").value;
-    document.getElementById("user-info").innerHTML = "Name: " + name + "<br>Username: " + username;
-    document.getElementById("display").style.display = "block";
+  let userInfo = "";
+  for (const user of users) {
+    userInfo += "Name: " + user.name + "<br>Username: " + user.username + "<br><br>";
+  }
+  
+  display.style.display = "block";
+  document.getElementById("user-info").innerHTML = userInfo;
 });
